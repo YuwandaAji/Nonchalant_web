9f804a2ca1b128e693489e08c4a5287340eb0fe2
Yuwanda aji <yuwandaaji657@gmail.com>
2025-11-14 09:03:30 +0700

    update detail kasir

--- js_detailkasir.js
+++ js_detailkasir.js
@@ -1,23 +1,56 @@
-const buttons = document.querySelectorAll('.btn-group .btn');
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll("#btn-bar .btn-custom");
+    let currentSts = "baru";
 
-buttons.forEach(btn => {
-  btn.addEventListener('click', () => {
-    // hapus status aktif dari semua tombol
-    buttons.forEach(b => b.classList.remove('active'));
+    function updateSts(newSts) {
+        currentSts = newSts;
+        
+        buttons.forEach((btn) => {
+            btn.classList.toggle("active", btn.dataset.status === newSts);
+        });
 
-    // kasih status aktif ke tombol yang diklik
-    btn.classList.add('active');
+        if (newSts === "diantar") {
+            printNota(currentSts);
+        }
+    }
 
-    // jalankan efek sesuai tombol
-    if (btn.id === "btn-prep") {
-      window.print(); // efek kayak "Print Nota"
-      console.log("Pesanan sedang disiapkan...");
+
+
+    buttons.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            updateSts(btn.dataset.status);
+        });
+    });
+
+    function printNota(currentSts) {
+        const notaWindow = window.open("", "", "width=400,height=600");
+        const htmlnota= `
+            <!DOCTYPE html>
+            <html>
+            <head><title>Nota Pesanan</title></head>
+            <body style="font-family: sans-serif; padding: 20px;">
+                <h2>Nota Pesanan #27729992</h2>
+                <p>Status: ${currentSts}</p>
+                <hr>
+                <p><strong>Nama:</strong> Qin Shi Huang</p>
+                <p><strong>Metode Pembayaran:</strong> Ovo (Lunas)</p>
+                <p><strong>Alamat:</strong> Jl. Ketintang</p>
+                <br>
+                <p>Terima kasih telah berbelanja!</p>
+            </body>
+            </html>
+        `;
+
+        console.log("Popup dibuat:", notaWindow);
+        notaWindow.document.open();
+        notaWindow.document.write(htmlnota);
+        notaWindow.document.close();
+        console.log("HTML nota sudah ditulis!");
+        notaWindow.print();
+    
+        
     }
-    if (btn.id === "btn-delivery") {
-      console.log("Pesanan sedang diantar...");
-    }
-    if (btn.id === "btn-done") {
-      console.log("Pesanan selesai ");
-    }
-  });
+
+    updateSts(currentSts);
+
 });
